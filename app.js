b882bb68d6a99d4a225f0865cf749541ef39c993
Tibor Toth <ttothuk@gmail.com>
2020-07-06 18:48:06 +0100

    Update app.js
    button function scope problem

--- app.js
+++ app.js
@@ -1,45 +1,47 @@
-window.addEventListener('load', ()=> {
-    let long;
-    let lat;
-    let temperatureDescription = document.querySelector('.temperature-description');
-    let temperatureDegree = document.querySelector('.temperature-degree');
-    let locationTimezone = document.querySelector('.timezone');
-    let iconclass = document.querySelector('.icon');
-    
+function buttonFunction() {
 
-    // App data
-    const weather = {};
-    const KELVIN = 273;
-    weather.temperature = {
-        unit : "celsius"
-    }
+    console.log("in scope");
+    let q = document.getElementById("inputCountryName").value;
+    console.log(q);
 
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(position =>{
-            //long = position.coords.longitude;
-            //lat = position.coords.latitude;                        
+    window.addEventListener('load', () => {   
+        
+        let q = document.getElementById("inputCountryName").value;
+        console.log(q);
 
-            const q = "Budapest";
-            const api_city = `https://api.openweathermap.org/data/2.5/weather?appid=57cb9da61fdd7b88043f4e59b148d268&q=${q}`;
-            
-            //const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=57cb9da61fdd7b88043f4e59b148d268`;
-            
-            fetch(api_city)
-            .then(response =>{
+        let temperatureDescription = document.querySelector('.temperature-description');
+        let temperatureDegree = document.querySelector('.temperature-degree');
+        let locationTimezone = document.querySelector('.timezone');
+        let iconclass = document.querySelector('.icon');
+
+
+        // App data
+        const weather = {};
+        const KELVIN = 273;
+        weather.temperature = {
+            unit: "celsius"
+        }
+
+        const api_city = `https://api.openweathermap.org/data/2.5/weather?appid=57cb9da61fdd7b88043f4e59b148d268&q=${q}`;
+
+        console.log(api_city);
+
+        fetch(api_city)
+            .then(response => {
                 return response.json();
             })
             .then(data => {
                 console.log(data);
-                
+
                 const temperature = Math.floor(data.main.temp - KELVIN);
                 const city = data.name;
                 const desc = data.weather[0].description
                 const icon = data.weather[0].icon;
-                
+
                 temperatureDegree.textContent = temperature;
                 temperatureDescription.textContent = desc;
                 locationTimezone.textContent = city;
-                
+
 
                 let image = document.createElement("img");
                 image.src = `./img/${icon}.png`;
@@ -49,13 +51,7 @@ window.addEventListener('load', ()=> {
                 image.style.width = '156px';
                 image.style.height = 'auto';
 
-                //console.log(q);
             })
-        });       
-    }    
-});
-
-function buttonFunction() {
-    let q = document.getElementById("buttonID").value;
-    console.log(q);
-}
+    }
+    );
+};
